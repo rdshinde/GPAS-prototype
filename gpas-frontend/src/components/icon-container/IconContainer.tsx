6ee7f08bfd51321baa266bfe35699f590f61ef2d
rishikesh.s <rdshinde2000@gmail.com>
2023-02-12 16:14:00 +0530

    Update IconContainer.tsx

--- gpas-frontend/src/components/icon-container/IconContainer.tsx
+++ gpas-frontend/src/components/icon-container/IconContainer.tsx
@@ -6,7 +6,7 @@ export const IconContainer = (props: Props) => {
   return (
     <section className="md:m-2 sm:m-1 xl:m-3 md:p-2 xl:p-3 sm:p-1 flex flex-row items-center justify-center cursor-pointer">
       <h1 className="flex items-center justify-center">
-        <span className="bg-indigo-500 opacity-80 sm:w-[50px] sm:h-[50px] xl:w-[75px] xl:h-[75px] p-2 rounded-full mx-3 relative -z-10  shadow-lg shadow-indigo-500">
+        <span className="bg-bluelight opacity-80 sm:w-[50px] sm:h-[50px] xl:w-[75px] xl:h-[75px] p-2 rounded-full mx-3 relative -z-10  shadow-lg shadow-indigo-500">
           <img
             src="logo.png"
             className="sm:w-[50px] xl:w-[75px] absolute left-1 -top-1 z-10 opacity-100 "
@@ -14,7 +14,7 @@ export const IconContainer = (props: Props) => {
             alt="logo"
           />
         </span>{" "}
-        <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 md:text-2xl sm:text-xl xl:text-3xl text-gradient-to-br hover:bg-gradient-to-r hover:from-pink-500 hover:via-purple-500 hover:to-indigo-500 drop-shadow-lg shadow-pink-800">
+        <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-bluelight via-blue to-bluelightest md:text-2xl sm:text-xl xl:text-3xl text-gradient-to-br hover:bg-gradient-to-r hover:from-bluelightest hover:via-blue hover:to-bluelight drop-shadow-lg shadow-gray-300">
           VisualDAuth
         </span>
       </h1>
