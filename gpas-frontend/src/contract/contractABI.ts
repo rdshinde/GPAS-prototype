4bf127646ff8af06401fdaca9d650cffa79b86ba
rishikesh.s <rdshinde2000@gmail.com>
2023-02-26 15:40:32 +0530

    env added

---
+++ gpas-frontend/src/contract/contractABI.ts
@@ -0,0 +1,6 @@
+export const developmentContractAddress =
+  process.env.DEVELOPMENT_CONTRACT_ADDRESS;
+
+export const productionContractAddress =
+  process.env.PRODUCTION_CONTRACT_ADDRESS;
+export const contractABI = [];
--- gpas-frontend/src/index.tsx
+++ gpas-frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { UiProvider } from "./context/ui/UiProvider";
 import { VisualDAuthProvider } from "./context/auth/VisualDAuthProvider";
 
 const root = ReactDOM.createRoot(
