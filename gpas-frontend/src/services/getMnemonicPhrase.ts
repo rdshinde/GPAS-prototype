0238951e229d4b1ce89074dfadfa0361f682eb32
rishikesh.s <rdshinde2000@gmail.com>
2023-02-28 13:15:14 +0530

    feat: getmnemonicPhrase

---
+++ gpas-frontend/src/services/getMnemonicPhrase.ts
@@ -0,0 +1,48 @@
+import Web3 from "web3";
+import {
+  contractABI,
+  productionContractAddress,
+} from "../contract/contractABI";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+const web3 = new Web3(window.ethereum);
+
+export const getMnemonicPhrase = async (
+  username: string,
+  walletAddress: string,
+  privateKey: string
+) => {
+  const account = await web3.eth.getAccounts().then((accounts) => accounts[0]);
+  const contract = new web3.eth.Contract(
+    contractABI,
+    productionContractAddress
+  );
+
+  if (!walletAddress && !privateKey && account) {
+    walletAddress = account;
+  }
+  try {
+    contract.methods
+      .getMnemonicPhrase(username)
+      .call(async (err: any, result: any) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          const resultObj = {
+            message: result[0],
+            status: result[1],
+            result: result[2],
+          };
+          console.log("mnemonicPhrase", resultObj);
+          return resultObj;
+        }
+      });
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
---
+++ gpas-frontend/src/services/loginRegisteredUser.ts
@@ -0,0 +1,54 @@
+import Web3 from "web3";
+import {
+  contractABI,
+  productionContractAddress,
+} from "../contract/contractABI";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+const web3 = new Web3(window.ethereum);
+
+export const loginRegisteredUser = async (
+  username: string,
+  password: string,
+  walletAddress: string,
+  privateKey: string
+) => {
+  const account = await web3.eth.getAccounts().then((accounts) => accounts[0]);
+  const contract = new web3.eth.Contract(
+    contractABI,
+    productionContractAddress
+  );
+
+  if (!walletAddress && !privateKey && account) {
+    walletAddress = account;
+  }
+  try {
+    contract.methods
+      .loginRegisteredUser(username, password)
+      .call(async (err: any, result: any) => {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log(result);
+          const resultObj = {
+            message: result[0],
+            result: result[1],
+            status: result[2],
+          };
+          return resultObj;
+        }
+      });
+  } catch (err: any) {
+    console.log(err.message);
+    const resultObj = {
+      message: err.message,
+      result: null,
+      status: false,
+    };
+    return resultObj;
+  }
+};
